7600883ddd22125af921963c1ed37c905dd104a5
João Merlin <loldinis@gmail.com>
2020-02-09 17:55:01 -0300

    first

---
+++ Resources/config.js
@@ -0,0 +1,39 @@
+
+// import required libs 
+var args = require('minimist') (process.argv.slice(2));
+var extend = require('extend');
+
+//Store the environment variable
+var environment = args.env || "test";
+
+//Common config... ie:name, version, max player etc..
+var common_conf = {
+    name: "mmo game server",
+    version: "0.0.1",
+    environment: environment,
+    max_player: 100,
+    data_paths: {
+        items: __dirname + "\\Game Data\\" + "Items\\",
+        maps: __dirname + "\\Game Data\\" + "Maps\\",
+    },
+    starting_zone: "rm_map_home"
+};
+
+//Environment Specific Configuration
+var conf = {
+    production: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8081,
+        database: "mongodb://127.0.0.1/mmo_prod"
+    },
+    test: {
+        ip: args.ip || "0.0.0.0",
+        port: args.port || 8082,
+        database: "mongodb://127.0.0.1/mmo_test"
+    }
+}
+
+extend(false, conf.production, common_conf)
+extend(false, conf.test, common_conf)
+
+module.exports = config = conf[environment];
